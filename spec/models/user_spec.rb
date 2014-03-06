7e7a302d3970b6beff7189fb0210364a05be191d
Eric Ho-Kee Chiu <eric.chiu@novartis.com>
2014-03-05 20:31:51 -0500

    Completed sec 6.4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,8 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:password_digest) }  
+  it { should respond_to(:authenticate) }  
+
   it { should be_valid }
   
   describe "when name is not present" do
@@ -81,4 +83,33 @@ describe User do
     
     it { should_not be_valid }
   end
+  
+  describe "password too short" do
+    before do
+      @user.password = @user.password_confirmation = "a"*5
+    end
+    
+    it { should be_invalid }
+  end
+
+  describe "return value of authentication method" do
+    before do
+      @user.save
+    end
+
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+    
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+
+  end
+
 end
