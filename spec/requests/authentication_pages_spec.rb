fc0df69acec03635f9ca2805fa906a7e4a5073d8
Eric Ho-Kee Chiu <eric.chiu@novartis.com>
2014-03-19 09:24:40 -0400

    Completed 9.2.1

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -35,6 +35,27 @@ describe "AuthenticationPages" do
       it { should_not have_link('Sign in', href: signin_path) }
     end
 
-  end
+  end # describe "signin" do
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
+  end # describe "authorization" do
+
 
 end
